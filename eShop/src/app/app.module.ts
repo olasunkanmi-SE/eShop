389736d8af019ba011442ce8251d7be6b55594c9
Raymond Oyinlola <olasunkanmi@Raymonds-MacBook-Pro.local>
2020-09-20 06:46:00 +0800

    define the environmental variables

--- eShop/src/app/app.module.ts
+++ eShop/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { CoreModule } from './modules/core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     CoreModule,
     MaterialModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
