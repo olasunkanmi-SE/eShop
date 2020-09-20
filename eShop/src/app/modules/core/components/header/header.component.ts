dece0f92d9b730368cfffd892af9759b275dfb7a
Raymond Oyinlola <olasunkanmi@Raymonds-MacBook-Pro.local>
2020-09-20 17:14:31 +0800

    create products mega menu

--- eShop/src/app/modules/core/components/header/header.component.ts
+++ eShop/src/app/modules/core/components/header/header.component.ts
@@ -18,5 +18,71 @@ export class HeaderComponent implements OnInit {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  productsCategory = [
+    {
+      categoryName: 'ElectronicsDevices',
+      children: [
+        {
+          type: 'Mobile & Tablets',
+          children: ['Mobiles', 'Landlines', 'Tablets'],
+        },
+        {
+          type: 'Laptop',
+          children: ['Traditional Laptops', 'Gaming Laptops'],
+        },
+        {
+          type: 'Console Gaming',
+          children: ['Console', 'Console Game', 'Console Game Accessories'],
+        },
+        {
+          type: 'Smart watches & Accessories',
+          children: ['Fitness Trackers', 'Smart Trackers'],
+        },
+      ],
+    },
+    {
+      categoryName: 'Electronics Acessories',
+      children: [
+        {
+          type: 'Mobile Accesories',
+          children: ['Phone Cases', 'Power Banks', 'Cables & Converters'],
+        },
+        {
+          type: 'Audios',
+          children: [
+            'Head Phones & Head Sets',
+            'Portable Speakers',
+            'Home Audio',
+          ],
+        },
+        {
+          type: 'Smart Devices',
+          children: [
+            'Electronic Cigarrets',
+            'Smart Speakers',
+            'Virtual Reality',
+          ],
+        },
+      ],
+    },
+    {
+      categoryName: "Men's' Fashion",
+      children: [
+        {
+          type: 'Men Clothing',
+          children: ['Tshirt & Tanks', 'Shirts', 'Pants'],
+        },
+        {
+          type: 'Men Shoes',
+          children: ['Shoes', 'Boots', 'Flip Flops'],
+        },
+        {
+          type: 'Accessories',
+          children: ['Socks', 'Belts', 'Hat & Caps'],
+        },
+      ],
+    },
+  ];
+
   ngOnInit(): void {}
 }
