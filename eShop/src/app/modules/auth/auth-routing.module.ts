96dd285d963adf8f4ca7328f5fd5fca08dc45729
raymond-frontend <raymond.oyinlola@gmail.com>
2020-10-14 00:39:31 +0800

    create the starter components

--- eShop/src/app/modules/auth/auth-routing.module.ts
+++ eShop/src/app/modules/auth/auth-routing.module.ts
@@ -1,12 +1,20 @@
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'signin', component: LoginComponent },
+  { path: 'forgotpassword', component: ForgotPasswordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
