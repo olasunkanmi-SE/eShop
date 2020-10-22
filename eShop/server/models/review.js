f0f2001c305ba12bc7b8273fef9963c2fd07f820
raymond-frontend <raymond.oyinlola@gmail.com>
2020-10-23 07:31:19 +0800

    Create the Product, Review and Order models

---
+++ eShop/server/models/review.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const reviewSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Review = model("Review", reviewSchema);
