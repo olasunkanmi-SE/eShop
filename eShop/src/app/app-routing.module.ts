c1e35d383c3f0c1d2d6d7a38e061f69648a309d7
raymond-frontend <raymond.oyinlola@gmail.com>
2020-10-27 17:55:55 +0800

    create child route for component modules

--- eShop/src/app/app-routing.module.ts
+++ eShop/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadChildren: () =>
       import('./modules/home/home.module').then((m) => m.HomeModule),
   },
--- eShop/src/app/modules/auth/auth-routing.module.ts
+++ eShop/src/app/modules/auth/auth-routing.module.ts
@@ -6,10 +6,15 @@ import { AuthComponent } from './auth.component';
 import { ForgotPasswordPageComponent } from './forgot-password/container/forgot-password-page.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent },
-  { path: 'register', component: SignupPageComponent },
-  { path: 'signin', component: LoginPageComponent },
-  { path: 'forgotpassword', component: ForgotPasswordPageComponent },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: 'register', component: SignupPageComponent },
+      { path: 'signin', component: LoginPageComponent },
+      { path: 'forgotpassword', component: ForgotPasswordPageComponent },
+    ],
+  },
 ];
 
 @NgModule({
