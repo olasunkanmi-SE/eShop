4e2b4b91e32dbf0b66cc07efb3a47dc95bf10c7b
ola <raymond.oyinlola@gmail.com>
2022-11-02 10:54:21 +0800

    add env file

--- eShop/server/config/db.js
+++ eShop/server/config/db.js
@@ -1,2 +1,1 @@
-export const dbUri =
-  "mongodb+srv://kosemani:omowunmi@cluster0.qj8ln.mongodb.net/shop?retryWrites=true&w=majority";
+export const dbUri = process.env.DBURL;
--- eShop/server/config/default.js
+++ eShop/server/config/default.js
@@ -1,3 +1,3 @@
 export const keys = {
-  jwtPrivateKey: "shop_privateKey",
+  jwtPrivateKey: process.env.JWTSECRET,
 };
